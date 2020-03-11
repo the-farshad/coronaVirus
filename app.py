6431cba5d5ea4c3699d3af6ec77ad8f5da99ab56
Farshad <Farshad.1991@gmail.com>
2020-03-11 22:04:13 +0300

    Project some AI and Effncincy

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ from datetime import datetime
 import os
 
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class Country(object):
     def __init__(self, country, confirmed=0, deaths=0, recovered=0):
         self.country = country
@@ -47,9 +58,14 @@ def scrape_confirmed_cases(confirmed_cases_results, countries_data):
             countries_data.update({
                 country_confirmed_list[1]: {
                     u'cityName': country_confirmed_list[1],
+                    u'countryName_KU': '',
+                    u'bearing': 0.5,
+                    u'latLong': [0,0],
+                    u'zoom': 3,
                     u'cases': int(country_confirmed_list[0].replace(",", "")),
                     u'death': 0,
                     u'treated': 0,
+                    u'priority': 999,
                     }})
 
     # Find all of cases are confirmed and return them
@@ -91,7 +107,7 @@ def scrape_death_cases(total_deaths_results, countries_data):
             u'death': us_death,
             })
 
-        countries_data['Mainland China'].update({
+        countries_data['China'].update({
             u'death': china_death,
             })
 
@@ -130,7 +146,7 @@ def scrape_recovered_cases(total_recovered_results, countries_data):
             u'treated': us_recovered,
             })
 
-        countries_data['Mainland China'].update({
+        countries_data['China'].update({
             u'treated': china_recovered,
             })
 
@@ -147,25 +163,50 @@ def store_data_in_firebase(countries_data):
         if info != {}:
             courser = info['cityName']
             if courser in countries_data:
-                try:
-                    if info != countries_data[courser]:
-                        print(u"DATA UPDATED ----->>> {} >> {}. ######".format(courser, info))
+                if info['cases'] != countries_data[courser]['cases'] or info['death'] != countries_data[courser]['death'] or info['treated'] != countries_data[courser]['treated']:
+                    latitude = countries_data[courser]['latLong'][0]
+                    longitude = countries_data[courser]['latLong'][1]
+                    try:
                         collection.document(id_).update({
                             u'cityName': countries_data[courser]['cityName'],
+                            u'countryName_KU': countries_data[courser]['countryName_KU'],
                             u'cases': countries_data[courser]['cases'],
                             u'death': countries_data[courser]['death'],
                             u'treated': countries_data[courser]['treated'],
+                            u'bearing': countries_data[courser]['bearing'],
+                            u'latLong': firestore.GeoPoint(latitude, longitude),
+                            u'priority': countries_data[courser]['priority'],
+                            u'zoom': countries_data[courser]['zoom'],
                             })
-                except:
-                    print(u'We have some problem!')
+                    except:
+                        print(u'We have some problem!')
+                    else:
+                        print(u"{}DATA UPDATED ----->>> {}{} >> {}. ######".format(
+                            Colors.WARNING,
+                            Colors.ENDC,
+                            courser,
+                            info
+                            ))
                 del countries_data[courser]
     for new_country in countries_data:
-        print(u"NEW COUNTRY ADDED ---->>> {} >> {}. *****".format(new_country['cityName'], new_country))
+        print(u"{}NEW COUNTRY ADDED ---->>>{} {} >> {}. *****".format(
+            Colors.FAIL,
+            Colors.ENDC,
+            new_country,
+            countries_data[new_country]
+            ))
+        latitude = countries_data[new_country]['latLong'][0]
+        longitude = countries_data[new_country]['latLong'][1]
         collection.add({
             u'cityName': countries_data[new_country]['cityName'],
             u'cases': countries_data[new_country]['cases'],
             u'death': countries_data[new_country]['death'],
             u'treated': countries_data[new_country]['treated'],
+            u'countryName_KU': countries_data[new_country]['countryName_KU'],
+            u'bearing': countries_data[new_country]['bearing'],
+            u'latLong': firestore.GeoPoint(latitude, longitude),
+            u'priority': countries_data[new_country]['priority'],
+            u'zoom': countries_data[new_country]['zoom'],
             })
     return True
 
@@ -187,13 +228,13 @@ def data_scraper(data_gathered):
     data_gathered = scrape_confirmed_cases(confirmed_cases_results, data_gathered)
 
     # Scrape all of of countries have confirmed death
-    total_deaths_results = driver.find_elements_by_xpath("//*[@id='ember76']")
+    total_deaths_results = driver.find_elements_by_xpath("//*[@id='ember83']")
 
     data_gathered = scrape_death_cases(total_deaths_results, data_gathered)
 
     # Scrape all of of countries have confirmed recovered
     total_recovered_results = \
-        driver.find_elements_by_xpath("//*[@id='ember90']")
+        driver.find_elements_by_xpath("//*[@id='ember97']")
 
     data_gathered = \
         scrape_recovered_cases(total_recovered_results, data_gathered)
@@ -204,7 +245,7 @@ def data_scraper(data_gathered):
     return data_gathered
 
 def check_data(info):
-
+    print(info)
     filename = 'information.json'
 
     if os.path.exists(filename):
@@ -212,13 +253,23 @@ def check_data(info):
         with open(filename, 'r') as f:
             datastore = json.load(f)
 
+
+        for field in datastore:
+            try:
+                info[field]['latLong'] = datastore[field]['latLong']
+                info[field]['countryName_KU'] = datastore[field]['countryName_KU']
+                info[field]['priority'] = datastore[field]['priority']
+            except:
+                print(u'{}{}{} ---> We can\'t find in this country in our CSV'.format(Colors.FAIL, field ,Colors.ENDC))
+            else:
+                print(u'{}{}{} ---> This Field is updated'.format(Colors.OKGREEN, field ,Colors.ENDC))
         if datastore == info:
             return False
 
         # Writing JSON data
     with open(filename, 'w') as f:
         json.dump(info, f)
-    return True
+    return info
 
 
 def main():
@@ -228,15 +279,18 @@ def main():
     # Main Function for gathering data
     countries_data = data_scraper(countries_data)
 
+    countries_data = check_data(countries_data)
+
+    # Show data will save in firestore
     print(u">>> ALL DATA GATHERED >>>\n{}\n\n".format(countries_data))
 
-    if check_data(countries_data):
-        # Store data in Firebase
+    if countries_data:
         if store_data_in_firebase(countries_data):
             print(u'\n>>> Firebase connection and operation successfully ended.\n# Last Updated at >>> {}.\n'.format(datetime.now()))
     else:
-       print(u'\n>>> Every thing is updated.\n# Last Checked at >>> {}.\n'.format(datetime.now()))
-
+        print(u'\n>>> Every thing is updated.\n# Last Checked at >>> {}.\n'.format(datetime.now()))
 
 if __name__ == "__main__":
     main()
+
+
