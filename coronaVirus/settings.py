1731af8de654f5f5ad86f80b1cb11278574a5ccb
the-farshad <farshad.1991@gmail.com>
2020-04-30 19:50:52 +0300

    change databde to Postgressql

--- coronaVirus/settings.py
+++ coronaVirus/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'coronaVirus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'coronaVirus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("CV_DB_NAME", ''),
+        'USER': os.environ.get("CV_DB_USER", ''),
+        'PASSWORD': os.environ.get("CV_DB_PASSWORD", ''),
+        'HOST': os.environ.get("CV_DB_HOST", ''),
+        'PORT': os.environ.get("CV_DB_PORT", ''),
     }
 }
 
